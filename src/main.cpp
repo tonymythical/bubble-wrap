b9728d66b4e04f62feed0b508eb1410cffb03607
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-01-15 04:24:26 +0000

    Added change color function ingame

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <bn_core.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
+#include <bn_keypad.h>
 
 int main()
 {
     bn::core::init();
-    bn::backdrop::set_color(bn::color(20, 20, 31));
+    bn::backdrop::set_color(bn::color(31, 12, 12));
 
     while (true)
     {
+        if (bn::keypad::a_pressed())
+        {
+            bn::backdrop::set_color(bn::color(1, 31, 1));
+        }
+
         bn::core::update();
     }
 }
