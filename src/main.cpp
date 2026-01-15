983d2349ad2fd309ff9be684903d3338d3b1c50f
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-01-15 05:47:00 +0000

    Added keypad functionality for blue and green background colors with special combination with both held.

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,23 @@ int main()
 
     while (true)
     {
-        if (bn::keypad::a_pressed())
+        if (bn::keypad::a_held())
         {
             bn::backdrop::set_color(bn::color(1, 31, 1));
         }
 
+        else if (bn::keypad::b_held())
+        {
+            bn::backdrop::set_color(bn::color(1, 1, 31));
+        }
+
+        else
+            (bn::backdrop::set_color(bn::color(31, 12, 12)));
+
+        if (bn::keypad::a_held() && bn::keypad::b_held())
+        {
+            bn::backdrop::set_color(bn::color(1, 15, 15));
+        }
         bn::core::update();
     }
 }
