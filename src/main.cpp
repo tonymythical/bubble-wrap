66f64895dd87e121a76c85dfdeeba3b8a738a85a
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-01-18 00:35:14 +0000

    Added smiley face

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,20 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_keypad.h>
+#include <bn_sprite_ptr.h>
+#include <bn_sprite_items_dot.h>
 
 int main()
 {
     bn::core::init();
     bn::backdrop::set_color(bn::color(31, 12, 12));
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(20, -10);
+    bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-20, -10);
+    bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(25, 30);
+    bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-25, 30);
+    bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(0, 35);
+
+
 
     while (true)
     {
