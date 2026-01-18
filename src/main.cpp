136abb6a8f133cd28bd0666ee89f242da435172d
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-01-18 12:45:39 +0000

    changed

--- src/main.cpp
+++ src/main.cpp
@@ -31,14 +31,14 @@ int main()
     // bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(30, 40);
     // bn::sprite_ptr myCircle9 = bn::sprite_items::dot.create_sprite(40, 40);
 
-    bn::vector<bn::sprite_ptr, 10> circles = {};
+    bn::vector<bn::sprite_ptr, 20> circles = {};
 
     for(int x = -40; x <= 40; x += 10) {
         bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(x, 40);
         BN_LOG("x value", x);
         
         bn::fixed degrees = bn::fixed(x * 4);
-        bn::fixed angle = degrees * bn::fixed(2) / 180;
+        bn::fixed angle = degrees * bn::fixed(20) / 360;
         
         myCircle.set_position(bn::sin(angle) * 40, bn::cos(angle) * 40);
         circles.push_back(myCircle);
